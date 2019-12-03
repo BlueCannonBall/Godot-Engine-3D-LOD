ceee2f6b7c8e24520e6cf8b0132380a71dec2993
BlueCannonBall <mmttiizz100@gmail.com>
2019-12-02 19:18:14 -0500

    Changed file structure and optimized scripts

--- lod.gd
+++ lod.gd
@@ -14,26 +14,29 @@ export(float) var cull_distance = 450 # Set to 0 to disable culling.
 onready var lod0_ref = get_node(lod0)
 onready var lod1_ref = get_node(lod1)
 onready var lod2_ref = get_node(lod2)
+var distance_to_lod = 0
 
 # LOD System
 func run_lod():
-	if get_viewport().get_camera().get_global_transform().origin.distance_to(get_global_transform().origin) < lod1_min_distance:
+	distance_to_lod = get_viewport().get_camera().get_global_transform().origin.distance_to(get_global_transform().origin)
+	if distance_to_lod < lod1_min_distance:
 		lod1_ref.visible = false
 		lod2_ref.visible = false
 		lod0_ref.visible = true
-	if lod1_ref != null and get_viewport().get_camera().get_global_transform().origin.distance_to(get_global_transform().origin) >= lod1_min_distance:
+	if lod1_ref != null and distance_to_lod >= lod1_min_distance:
 		lod0_ref.visible = false
 		lod2_ref.visible = false
 		lod1_ref.visible = true
-	if lod2_ref != null and get_viewport().get_camera().get_global_transform().origin.distance_to(get_global_transform().origin) >= lod2_min_distance:
+	if lod2_ref != null and distance_to_lod >= lod2_min_distance:
 		lod0_ref.visible = false
 		lod1_ref.visible = false
 		lod2_ref.visible = true
-	if cull_distance != 0 and get_viewport().get_camera().get_global_transform().origin.distance_to(get_global_transform().origin) >= cull_distance:
+	if cull_distance != 0 and distance_to_lod >= cull_distance:
 		lod0_ref.visible = false
 		lod1_ref.visible = false
 		lod2_ref.visible = false
 
 # _Process
+# warning-ignore:unused_argument
 func _process(delta):
 	run_lod() # Always keep this in _process for LOD to compute.
